efe60f20c0904adbe7f4869b01ac02e3f9019d00
ayush <ayushanand754@gmail.com>
2026-07-28 14:29:27 +0530

    add register controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -8,7 +8,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (localFilePath: any) => {
+const uploadOnCloudinary = async (localFilePath) => {
     try{
         if(!localFilePath) return null;
     
